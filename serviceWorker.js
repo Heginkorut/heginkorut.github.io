43c1a101656065ad1c8cef299a8181f8b8ef9fe2
Heginkorut <114905854+Heginkorut@users.noreply.github.com>
2022-10-04 14:17:49 +0300

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -22,10 +22,4 @@ self.addEventListener("install", installEvent => {
     })
   )
 })
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
-    })
-  )
-})
+
